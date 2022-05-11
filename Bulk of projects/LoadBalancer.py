8e8eaf965c58a1ecbf0f21a8ae54088d688cd863
HadesShade <54205634+HadesShade@users.noreply.github.com>
2022-05-11 11:38:04 +0700

    Add failover capability and logging feature
    Add failover capability in the algorithm in case the connection to first choosen backend server can't be established. Choose next server in the sorted form and so on.
    
    Add logging feature. Logs are written to /var/log/rtrtr-lb.log

--- Bulk of projects/LoadBalancer.py
+++ Bulk of projects/LoadBalancer.py
@@ -1,13 +1,15 @@
-import socket,sys,select,random,os,select
+import socket,sys,select,random,os,select,collections
+from Logger import *
 
 def openConfigurationFile():
 	listServers = dict()
 	file = open("/etc/rtrtr-lb.conf","r")
-	servers = file.readlines()
+	servers = file.read().splitlines()
 	for server in servers:
 		ip = str(server.split(':')[0])
 		port = int(server.split(':')[1])
 		listServers[ip] = port
+	LogInfo(f"Backend Servers Loaded : {listServers}")
 	return listServers
 
 def initializeFlowsCount():
@@ -15,6 +17,7 @@ def initializeFlowsCount():
 	listServers = openConfigurationFile()
 	for ip in listServers:
 		fCounts[ip] = 0
+	LogInfo(f"Initialize Connection Count for Every Backend Server : {fCounts}")
 	return fCounts
 
 flowsCount = initializeFlowsCount()
@@ -24,20 +27,14 @@ all_sockets = list()
 def leastConn():
 	connectionCount = list()
 	choosenServers = list()
-	for server in flowsCount:
-		connectionCount.append(int(flowsCount[server]))
-	least = min(connectionCount)
+	choosenPorts = list()
+	sortedCount = collections.OrderedDict(flowsCount)
+	for server in sortedCount:
+		connectionCount.append(int(sortedCount[server]))
+		choosenServers.append(str(server))
+		choosenPorts.append(int(openConfigurationFile()[server]))
 
-	for server, count in flowsCount.items():
-		if count == least:
-			choosenServers.append(server)
-
-	return random.choice(choosenServers)
-
-def getServer():
-	ip = leastConn()
-	port = openConfigurationFile()[ip]
-	return ip,port
+	return connectionCount, choosenServers, choosenPorts
 
 def startLB():
 	listenSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -63,20 +60,36 @@ def startLB():
 
 def onConnectionAccept(listenSocket):
 	client_socket, client_ip = listenSocket.accept()
-	server_ip, server_port = getServer()
+	server_connections, server_ips, server_ports = leastConn()
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	try:
-		server_socket.connect((server_ip,server_port))
-	except Exception:
-		client_socket.close()
 
-	all_sockets.append(client_socket)
-	all_sockets.append(server_socket)
-
-	flowsPair[client_socket] = server_socket
-	flowsPair[server_socket] = client_socket
-
-	flowsCount[server_ip] += 1
+	for i in range (len(server_connections)):
+		if i == len(server_connections) - 1:
+			try:
+				server_socket.connect((server_ips[i],server_ports[i]))
+				all_sockets.append(client_socket)
+				all_sockets.append(server_socket)
+				flowsPair[client_socket] = server_socket
+				flowsPair[server_socket] = client_socket
+				flowsCount[server_ips[i]] += 1
+				LogInfo(f"Established Connection from {client_socket.getpeername()[0]} to {server_ips[i]}")
+				break
+			except Exception:
+				LogError("No Connection to Backend Servers can be Established")
+				client_socket.close()
+		else:
+			try:
+				server_socket.connect((server_ips[i],server_ports[i]))
+				all_sockets.append(client_socket)
+				all_sockets.append(server_socket)
+				flowsPair[client_socket] = server_socket
+				flowsPair[server_socket] = client_socket
+				flowsCount[server_ips[i]] += 1
+				LogInfo(f"Established Connection from {client_socket.getpeername()[0]} to {server_socket.getpeername()[0]}")
+				break
+			except Exception:
+				LogError(f"Connection to {server_ips[i]} failed. Trying next server ...")
+				continue
 
 def onDataReceive(client_socket, data):
 	flowsPair[client_socket].send(data)
@@ -102,6 +115,8 @@ def onConnectionClose(socket):
 
 	else:
 		pass
+
+	LogInfo(f"Connection from {pair_ip1} to {pair_ip2} terminated gracefully.")
 
 if __name__ == "__main__":
 	try:
